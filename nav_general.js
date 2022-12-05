815678342946f3474c57befd416ce3c594efa0f0
Ksyusha <74423375+ksen4ik@users.noreply.github.com>
2022-12-05 16:16:35 +0300

    Update nav_general.js

--- nav_general.js
+++ nav_general.js
@@ -1,9 +1,9 @@
 document.write('\
   <div class="container-fluid">\
       <div class="b-header__logo">\
-          <a href="index.html" class="navbar-brand p-0">\
+          <a href="/index.html" class="navbar-brand p-0">\
               <img \
-              src="images/StyleRoom_logo.png" \
+              src="/images/StyleRoom_logo.png" \
               class="b-header__logo-img" \
               alt="Logo Style Room"\
               width="150">\
