2f110f15538dc417c0818a2a4c5e9cd2d2f835ae
Ksyusha Kotova <8150158.kk@gmail.com>
2022-12-05 15:01:09 +0300

    test-project-shop
    test web-page HTML+CSS

---
+++ js/login.js
@@ -0,0 +1,29 @@
+document.write('\
+  <div class="modal-dialog modal-sm">\
+    <div class="modal-content">\
+      <div class="modal-header sign-up-header">\
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Log in to your account</h1>\
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>\
+      </div>\
+      <form class="row g-2">\
+        <div class="mb-3">\
+          <label for="exampleDropdownFormEmail2" class="form-label">Email address</label>\
+          <input type="email" class="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com">\
+        </div>\
+        <div class="mb-3">\
+          <label for="exampleDropdownFormPassword2" class="form-label">Password</label>\
+          <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Password">\
+        </div>\
+        <div class="mb-3">\
+          <div class="form-check">\
+            <input type="checkbox" class="form-check-input" id="dropdownCheck2">\
+            <label class="form-check-label" for="dropdownCheck2">\
+              Remember me\
+            </label>\
+          </div>\
+        </div>\
+        <button type="submit" class="btn btn-primary">Sign in</button>\
+      </form>\
+    </div>\
+  </div>\
+');
---
+++ js/registration.js
@@ -0,0 +1,58 @@
+document.write('\
+  <div class="modal-dialog modal-lg">\
+    <div class="modal-content">\
+      <div class="modal-header sign-up-header">\
+        <h1 class="modal-title fs-4" id="exampleModalLabel">Quick Registration</h1>\
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>\
+      </div>\
+      <form class="row g-3">\
+        <div class="col-md-3">\
+          <label for="validationDefault01" class="form-label">First name</label>\
+          <input type="text" class="form-control" id="validationDefault01" value="" required>\
+        </div>\
+        <div class="col-md-3">\
+          <label for="validationDefault02" class="form-label">Last name</label>\
+          <input type="text" class="form-control" id="validationDefault02" value="" required>\
+        </div>\
+        <div class="col-md-3">\
+          <label for="validationDefault03" class="form-label">Country</label>\
+          <select class="form-select" id="validationDefault03" required>\
+            <option selected disabled value="">Choose...</option>\
+            <option>Belarus</option>\
+            <option>Great Britain</option>\
+            <option>USA</option>\
+          </select>\
+        </div>\
+        <div class="col-md-3">\
+          <label for="validationDefault04" class="form-label">City</label>\
+          <input type="text" class="form-control" id="validationDefault04" required>\
+        </div>\
+        <div class="col-md-3">\
+          <label for="validationDefaultUsername" class="form-label">Username</label>\
+          <div class="input-group">\
+            <span class="input-group-text" id="inputGroupPrepend2">@</span>\
+            <input type="text" class="form-control" id="validationDefaultUsername"  aria-describedby="inputGroupPrepend2" required>\
+          </div>\
+        </div>\
+        <div class="col-md-4">\
+          <label for="validationDefault05" class="form-label">E-mail address</label>\
+          <input type="text" class="form-control" id="validationDefault05" required>\
+        </div>\
+        <div class="col-md-3">\
+          <label for="validationDefault06" class="form-label">Password</label>\
+          <input type="text" class="form-control" id="validationDefault06" required>\
+        </div>\
+        <div class="col-12">\
+          <div class="form-check">\
+            <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>\
+            <label class="form-check-label" for="invalidCheck2">\
+              Agree to terms and conditions\
+            </label>\
+          </div>\
+        </div>\
+        <div class="col-12">\
+          <button class="btn btn-primary" type="submit">Submit form</button>\
+        </div>\
+    </div>\
+  </div>\
+');
