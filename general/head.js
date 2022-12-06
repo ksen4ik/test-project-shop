e2b44bb1dd46f6e9e6bd408a953e7b811b9b10c9
Ksyusha <74423375+ksen4ik@users.noreply.github.com>
2022-12-06 14:44:59 +0300

    Create head.js

---
+++ general/head.js
@@ -0,0 +1,22 @@
+document.write('\
+    <meta charset="UTF-8"> <!--кодировка-->\
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--показывать по всей ширине экрана-->\
+    <title>Style Room</title>\
+    \
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" \
+    rel="stylesheet" \
+    integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" \
+    crossorigin="anonymous">\
+    \
+    <script \
+    src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" \
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" \
+    crossorigin="anonymous">\
+    </script>\
+    \
+    <script \
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" \
+    integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" \
+    crossorigin="anonymous">\
+    </script>\
+');
